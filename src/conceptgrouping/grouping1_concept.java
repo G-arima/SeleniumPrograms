0d136e7802526754bba460de541ab902cb5ad73d
G-arima <garima.lekhi1994@outlook.com>
2024-09-30 19:15:59 +0530

    Assignment 142

---
+++ src/conceptgrouping/grouping1_concept.java
@@ -0,0 +1,36 @@
+package conceptgrouping;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class grouping1_concept {
+	
+	@Test(groups= {"smoke"})
+	public void testcase1()
+	{
+		
+	}
+	@Test(groups= {"smoke","system","regression"})
+	public void testcase2()
+	{
+		
+	}
+	@Test(groups= {"system"})
+	public void testcase3()
+	{
+		
+	}
+	@Test(groups= {"system"})
+	public void testcase4()
+	{
+		
+	}
+	@Test(groups= {"system"})
+	public void testcase5()
+	{
+		
+	}
+
+}
